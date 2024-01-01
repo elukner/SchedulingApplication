33691b92634847d45d51adc1021a969eeaf9d8b1
elukner <elukner@gmail.com>
2024-01-01 10:16:55 -0700

    Added notes DAO classes in the dao package.

--- src/dao/CustomerDAO.java
+++ src/dao/CustomerDAO.java
@@ -1,4 +1,28 @@
 package dao;
 
+/**
+ *The dao package will hold Utility Classes that handle Database Access.
+ *
+ * These classes interact with the database,
+ * perform CRUD operations, and handle data retrieval.
+ *
+ * Responsibility: The dao package primarily focuses on encapsulating
+ * the logic associated with database interactions.
+ *
+ * Purpose: It contains classes responsible for database-related operations,
+ * such as CRUD (Create, Read, Update, Delete)
+ * operations, querying the database, and managing
+ * data retrieval and storage.
+ *
+ * Components: Typically includes classes that abstract
+ * database operations, handle connections, execute queries,
+ * and perform data manipulation.
+ *
+ * Usage: DAO classes are specific to interacting with your
+ * database tables/entities. They map Java objects to
+ * database entities and handle the
+ * interaction between the application and the database.
+ **/
+
 public class CustomerDAO {
 }
