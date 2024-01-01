ea8a8c3e913bfc295e4eda6b5a1861f9819d20f9
elukner <elukner@gmail.com>
2024-01-01 10:13:51 -0700

    Added New DAO classes that reflect utility classes responsible for database access.

--- src/controller/CustomerRecordController.java
+++ src/controller/CustomerRecordController.java
@@ -1,4 +1,10 @@
 package controller;
 
+/**
+ *The controller package will hold Controller
+ * classes that contain Business Logic for your views.
+ *
+ **/
+
 public class CustomerRecordController {
 }
--- src/dao/DatabaseAccessUtil.java
+++
@@ -1,9 +0,0 @@
-package dao;
-/**
- * The dao package will hold Utility Classes
- * that handle Database Access.
- *
- *
- */
-public class DatabaseAccessUtil {
-}
