0e622b44c6c472829cd12766bf7a002985ede808
elukner <elukner@gmail.com>
2024-01-02 18:20:35 -0700

    Created Log in window.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,10 +14,18 @@ import java.sql.SQLException;
 
 public class Main extends Application {
 
+//    @Override
+//    public void start(Stage primaryStage) throws Exception{
+//       Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        primaryStage.setTitle("Hello World");
+//        primaryStage.setScene(new Scene(root, 300, 275));
+//        primaryStage.show();
+//    }
+
+//tester
     @Override
     public void start(Stage primaryStage) throws Exception{
-       Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        //Parent root = FXMLLoader.load(getClass().getResource("view/scheduling.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../view/login.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
