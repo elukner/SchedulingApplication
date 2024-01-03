9d7dfbe6f508535859d1d81547b1e47b0e01405b
elukner <elukner@gmail.com>
2024-01-02 18:56:01 -0700

    Created and started working on scheduling view.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -25,9 +25,9 @@ public class Main extends Application {
 //tester
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../view/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../view/scheduling.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
