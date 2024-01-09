7ca19fce336acf0d928b70514c138118d0a829c2
elukner <elukner@gmail.com>
2024-01-08 17:43:59 -0700

    Made new Application start up class.

---
+++ src/SchedulingApplication.java
@@ -0,0 +1,24 @@
+import helper.JDBC;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SchedulingApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("view/login.fxml"));
+        //   primaryStage.setTitle("Scheduling Application");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        JDBC.openConnection();
+        launch(args);
+        JDBC.closeConnection();
+    }
+}
