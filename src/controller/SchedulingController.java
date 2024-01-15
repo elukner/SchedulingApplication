c54e8859a9146546f06684902b74192c29928452
elukner <elukner@gmail.com>
2024-01-15 14:49:30 -0700

    Implemented basic add apointment funcitonality except for startDateAndTimeTxt.getText(),endDateAndTimeTxt.getText() still not accruate.

--- src/controller/SchedulingController.java
+++ src/controller/SchedulingController.java
@@ -204,7 +204,7 @@ public class SchedulingController extends Application implements Initializable {
     }
 
     /**
-     * TODO
+     * TODO startDateAndTimeTxt.getText(),endDateAndTimeTxt.getText()
      * @param event
      */
     @FXML
