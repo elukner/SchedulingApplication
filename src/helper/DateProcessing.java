0af117fac7fc7c377948f8e0dea09ba7a52179ad
elukner <elukner@gmail.com>
2024-01-26 10:10:26 -0700

    Added and updated javadoc comments in the DateProcessing class in the helper package.  Also, deleted unused code from this class.

--- src/helper/DateProcessing.java
+++ src/helper/DateProcessing.java
@@ -10,81 +10,46 @@ package helper;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAccessor;
 
 /**
- * And the helper package will hold helper classes
- * that handle tasks such as
- * Date / Time processing, List management, etc.
- *
- * Add a utility or utils package to hold
- * classes for your; Database Connection,
- * Query Execution, Collection (ObservarbleArrayList)
- * Management, Functional Interfaces, and time conversion files.
- *
- * Date/Time Operations: Conversions between different time zones,
- * date formatting, or any other date/time-related functionalities.
+ * The DateProcessing class provides utility methods for processing and formatting dates.
  */
-
 public class DateProcessing {
-    public static String getMonth(String dateTime){
-        //Getting the LocalDateTime Objects from String values
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd kk:mm:ss");
-        String startTimeTxt = dateTimeFormatter.format(LocalDateTime.now());
 
-        LocalDateTime ldtStart = LocalDateTime.parse(startTimeTxt, dateTimeFormatter);
-
-
-        //Getting the day of the week
-        //System.out.println(ldtStart.getDayOfWeek());
-        return ldtStart.getMonth().toString();
-    }
-
-    public static LocalDate getCurrentMonth(){
-        //Showing how to parse the Date/Time String
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        String startTimeTxt = dateFormatter.format(LocalDateTime.now());
-        LocalDate localDate = LocalDate.parse(startTimeTxt, dateFormatter);
-        // System.out.println("The local date is " + localDate);
-
-
-        //Getting the day of the week
-        //System.out.println(ldtStart.getDayOfWeek());
-        return localDate;
-    }
-
+    /**
+     * Parses a date string in the format "yyyy-MM-dd" and returns a LocalDate object.
+     *
+     * @param dateTime The date string to be parsed.
+     * @return A LocalDate object representing the parsed date.
+     */
     public static LocalDate getDate(String dateTime){
-
-        //Showing how to parse the Date/Time String
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDate localDate = LocalDate.parse(dateTime.substring(0, 10), dateTimeFormatter);
-        // System.out.println("The local date is " + localDate);
-
-
-        //Getting the day of the week
-        //System.out.println(ldtStart.getDayOfWeek());
         return localDate;
     }
 
+    /**
+     * Parses a date-time string in the format "yyyy-MM-dd HH:mm:ss" and returns a LocalDate object.
+     *
+     * @param dateTime The date-time string to be parsed.
+     * @return A LocalDate object representing the date part of the parsed date-time.
+     */
     public static LocalDate getDateFromDateTime(String dateTime){
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         LocalDateTime localDateTime = LocalDateTime.parse(dateTime, dateTimeFormatter);
-
         return LocalDate.of(localDateTime.getYear(),localDateTime.getMonth(),localDateTime.getDayOfMonth());
     }
 
+    /**
+     * Parses a date string in the format "yyyy-MM-dd" and returns a LocalDate object.
+     *
+     * @param dateTime The date string to be parsed.
+     * @return A LocalDate object representing the parsed date.
+     */
     public static LocalDate getFormatedDate(String dateTime){
-
-        //Showing how to parse the Date/Time String
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDate localDate = LocalDate.parse(dateTime, dateTimeFormatter);
-        // System.out.println("The local date is " + localDate);
-
-
-        //Getting the day of the week
-        //System.out.println(ldtStart.getDayOfWeek());
         return localDate;
     }
 }
